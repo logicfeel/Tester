44901f02d7e4e22b0e290962e0ec1eb5c6d77c62
logic <logicfeel@nate.com>
2022-11-22 14:23:27 +0900

    템플릿 import, export 기본 체크 완료

---
+++ TempA/Template.js
@@ -0,0 +1,103 @@
+var handlebars = require('handlebars');
+var handlebarsWax = require('handlebars-wax');
+
+class TemplateA {
+    wax;
+    src = '{{lorem}} {{ipsum}}  {{#>far}} boo 없음 {{/far}} .. key:'
+    constructor() {
+        this.build();
+    }
+
+    build() {
+        this.wax = handlebarsWax(handlebars.create())    // 독립 실행
+            // Partials
+            .partials('./partials/**/*.{hbs,js}')
+            .partials({
+                boo: '{{#each boo}}{{greet}}{{/each}}',
+                far: '{{#each far}}{{length}}{{/each}}',
+            })
+            // Helpers
+            .helpers('./helpers/**/*.js')
+            .helpers({
+                foo: function () { return 'FOO' },
+                bar: function () { return 'BAR' }
+            })
+            // Data
+            .data('./data/**/*.{js,json}')
+            .data({
+                lorem: 'dolor',
+                ipsum: 'sit amet'
+        });
+    }
+    export(key) {
+        var _this = this;
+        return function() {
+            var template = _this.wax.compile(_this.src+ key);
+            return template();
+        }
+    }
+    run() {
+        var template = this.wax.compile(this.src);
+        return template();
+    }
+}
+
+
+var ta = new TemplateA()
+
+class TemplateB {
+    wax;
+    src = '{{lorem}} {{ipsum}}  {{#>far}} boo 없음 {{/far}} .. <{{>super}}>  key:'
+    constructor() {
+        this.build();
+
+        this.wax.partials()
+    }
+
+    build() {
+        this.wax = handlebarsWax(handlebars.create())    // 독립 실행
+            // Partials
+            .partials('./partials/**/*.{hbs,js}')
+            .partials({
+                boo: '{{#each boo}}{{greet}}{{/each}}',
+                far: '{{#each far}}{{length}}{{/each}}',
+                super: ta.export('SUPER')
+            })
+            // Helpers
+            .helpers('./helpers/**/*.js')
+            .helpers({
+                foo: function () { return 'FOO' },
+                bar: function () { return 'BAR' }
+            })
+            // Data
+            .data('./data/**/*.{js,json}')
+            .data({
+                lorem: '___dolor',
+                ipsum: '___sit amet'
+        });
+    }
+    import(obj) {
+
+        // obj.
+        // var _this = this;
+        // return function() {
+        //     var template = _this.wax.compile('{{lorem}} {{ipsum}}  {{#>far}} boo 없음 {{/far}} .. key:'+ key);
+        //     return template();
+        // }
+    }
+
+    run() {
+        var template = this.wax.compile(this.src);
+        return template();
+    }
+}
+
+
+
+// var fun = ta.export('Test')
+
+// console.log(fun());
+console.log(ta.run());
+
+var tb = new TemplateB()
+console.log(tb.run());
